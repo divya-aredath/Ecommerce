a5773b0c1211e8d189e77731ca8af7c03f5d1712
divya <divyat.730@gmail.com>
2026-05-01 22:31:15 +0400

    prevents static generation  code added

--- app/services/productservice.ts
+++ app/services/productservice.ts
@@ -2,7 +2,9 @@ export class ProductService {
 static getProducts=async()=>{{
     const url = "https://fakestoreapi.com/products";
     console.log("Fetching:", url);
-    const response = await fetch(url);
+    const response = await fetch(url,{
+  cache: "no-store"
+});
     console.log(response);
     if (!response.ok) throw new Error("Failed to fetch products");
     return response.json();
@@ -11,7 +13,9 @@ static getProducts=async()=>{{
 static async getProductDetails({ id }: { id: string }) {
     const url = `https://fakestoreapi.com/products/${id}`;
     console.log("Fetching:", url);
-    const response = await fetch(url);
+    const response = await fetch(url{
+  cache: "no-store"
+});
     console.log(response);
     if (!response.ok) throw new Error("Failed to fetch product");
     return response.json();
