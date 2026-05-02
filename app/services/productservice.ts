f816efc176844a6a913387eee87bd6bfc7255b87
divya <divyat.730@gmail.com>
2026-05-02 16:39:04 +0400

    try catch added

--- app/services/productservice.ts
+++ app/services/productservice.ts
@@ -1,24 +1,37 @@
+import { error } from "console";
+
 export class ProductService {
-static getProducts=async()=>{{
-    const url = "https://fakestoreapi.com/products";
-    console.log("Fetching:", url);
-    const response = await fetch(url,{
-  cache: "no-store"
-});
-    console.log(response);
-    if (!response.ok) throw new Error("Failed to fetch products");
-    return response.json();
-}
+   static getProducts=async()=>{
+      try {
+              const url = "https://fakestoreapi.com/products";
+              const response = await fetch(url,{
+            cache: "no-store"
+          });
+         if (!response.ok)
+          {
+                throw new Error("Failed to fetch products");
+          }
+         return await response.json();
+      }catch (err) 
+      {  
+                    console.error("getProducts error:", error);
+                    return []; // ✅ prevents crash
+      }
+     
 }
 static async getProductDetails({ id }: { id: string }) {
-    const url = `https://fakestoreapi.com/products/${id}`;
-    console.log("Fetching:", url);
-    const response = await fetch(url,{
-  cache: "no-store"
-});
-    console.log(response);
-    if (!response.ok) throw new Error("Failed to fetch product");
-    return response.json();
+  try {
+                const url = `https://fakestoreapi.com/products/${id}`;
+                const response = await fetch(url,{
+              cache: "no-store"
+            });
+    if (!response.ok) 
+      {throw new Error("Failed to fetch product");}
+    return  await response.json();
+  } catch (error) {
+    console.error("getProductDetails error:", error);
+    return null; // ✅ prevents crash
+  }
 }
 
 }
