9f3eff66f801630a7b152462cba8b7748cf95e80
divya <divyat.730@gmail.com>
2026-05-02 16:38:46 +0400

    data not found added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,13 +2,16 @@
 import { ProductService } from "@/app/services/productservice";
 import "./id.module.css";
 import ProductDetails from "@/app/components/ProductDetails";
-export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
+export default async function ProductPage({ params }: { params: { id: string } }) {
     try {
         const {id} = await params;
         const data = await ProductService.getProductDetails({ id });
         console.log(data);
+        if (!data) {
+              return <div>Product not found</div>;
+          }
        
-        return <ProductDetails product={data} />;
+        return <ProductDetails product={data}/>;
     } catch (err) {
         console.error("Product fetch failed:", err);
         return <div>Failed to load product</div>;
